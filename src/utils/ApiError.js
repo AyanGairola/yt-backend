e97f8c60ad4c715f87e71ea45711892bd27a8fc1
Ayan Gairola <ayangairola@Ayans-MacBook-Pro.local>
2024-03-08 23:16:46 +0530

    Fixed a typo in API ERROR

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,7 +1,7 @@
 class ApiError extends Error{
     constructor(statusCode,message="Something went Wrong",errors=[]){
         super(message)
-        this,statusCode=statusCode
+        this.statusCode=statusCode
         this.data=null
         this.message=message
         this.success= false
